eacb6fe2c9a33f480016c6095a83184ac42fc268
supriyamn <supriyamn13@gmail.com>
2020-09-22 16:55:29 +0530

    first commit

---
+++ AgeCalculator/AgeCalculator/Controllers/AgeCalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgeCalculator.Controllers
+{
+    public class AgeCalculatorController : Controller
+    {
+        // GET: AgeCalculator
+        public ActionResult Calculation()
+        {
+            return View();
+        }
+    }
+}
